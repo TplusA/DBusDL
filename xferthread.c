faaf6e140b1d41cef64d5eeb2a1ebcb6836c11b7
Robert Tiemann <r.tiemann@ta-hifi.com>
2015-08-28 11:15:35 +0200

    Better error reporting on D-Bus and in logs.
    Change-Id: I5f9145842952ccff0057faa358dd6b6d0b1c4c15

--- xferthread.c
+++ xferthread.c
@@ -133,6 +133,119 @@ static void close_and_remove(FILE *output_file, const char *filename)
     remove_file(filename);
 }
 
+static enum DBusListsErrorCode map_curl_error_to_list_error(CURLcode error)
+{
+    switch(error)
+    {
+      case CURLE_OK:
+        return LIST_ERROR_OK;
+
+      case CURLE_ABORTED_BY_CALLBACK:
+        return LIST_ERROR_INTERRUPTED;
+
+      case CURLE_WRITE_ERROR:
+      case CURLE_READ_ERROR:
+      case CURLE_FILE_COULDNT_READ_FILE:
+      case CURLE_SSL_CACERT_BADFILE:
+      case CURLE_SSL_CRL_BADFILE:
+        return LIST_ERROR_PHYSICAL_MEDIA_IO;
+
+      case CURLE_COULDNT_RESOLVE_PROXY:
+      case CURLE_COULDNT_RESOLVE_HOST:
+      case CURLE_COULDNT_CONNECT:
+      case CURLE_FTP_ACCEPT_TIMEOUT:
+      case CURLE_OPERATION_TIMEDOUT:
+      case CURLE_LDAP_CANNOT_BIND:
+      case CURLE_INTERFACE_FAILED:
+      case CURLE_SEND_ERROR:
+      case CURLE_RECV_ERROR:
+      case CURLE_TFTP_NOTFOUND:
+      case CURLE_REMOTE_DISK_FULL:
+      case CURLE_REMOTE_FILE_EXISTS:
+      case CURLE_REMOTE_FILE_NOT_FOUND:
+      case CURLE_SSH:
+        return LIST_ERROR_NET_IO;
+
+      case CURLE_UNSUPPORTED_PROTOCOL:
+      case CURLE_URL_MALFORMAT:
+      case CURLE_NOT_BUILT_IN:
+      case CURLE_FTP_WEIRD_SERVER_REPLY:
+      case CURLE_FTP_ACCEPT_FAILED:
+      case CURLE_FTP_WEIRD_PASS_REPLY:
+      case CURLE_FTP_WEIRD_PASV_REPLY:
+      case CURLE_FTP_WEIRD_227_FORMAT:
+      case CURLE_FTP_COULDNT_SET_TYPE:
+      case CURLE_PARTIAL_FILE:
+      case CURLE_FTP_COULDNT_RETR_FILE:
+      case CURLE_QUOTE_ERROR:
+      case CURLE_HTTP_RETURNED_ERROR:
+      case CURLE_FTP_PORT_FAILED:
+      case CURLE_FTP_COULDNT_USE_REST:
+      case CURLE_RANGE_ERROR:
+      case CURLE_HTTP_POST_ERROR:
+      case CURLE_BAD_DOWNLOAD_RESUME:
+      case CURLE_LDAP_SEARCH_FAILED:
+      case CURLE_TOO_MANY_REDIRECTS:
+      case CURLE_TELNET_OPTION_SYNTAX:
+      case CURLE_GOT_NOTHING:
+      case CURLE_BAD_CONTENT_ENCODING:
+      case CURLE_LDAP_INVALID_URL:
+      case CURLE_FILESIZE_EXCEEDED:
+      case CURLE_SEND_FAIL_REWIND:
+      case CURLE_TFTP_ILLEGAL:
+      case CURLE_TFTP_UNKNOWNID:
+      case CURLE_SSL_SHUTDOWN_FAILED:
+      case CURLE_FTP_PRET_FAILED:
+      case CURLE_RTSP_CSEQ_ERROR:
+      case CURLE_RTSP_SESSION_ERROR:
+      case CURLE_FTP_BAD_FILE_LIST:
+        return LIST_ERROR_PROTOCOL;
+
+      case CURLE_REMOTE_ACCESS_DENIED:
+      case CURLE_UPLOAD_FAILED:
+      case CURLE_SSL_CONNECT_ERROR:
+      case CURLE_PEER_FAILED_VERIFICATION:
+      case CURLE_SSL_CERTPROBLEM:
+      case CURLE_SSL_CIPHER:
+      case CURLE_SSL_CACERT:
+      case CURLE_USE_SSL_FAILED:
+      case CURLE_LOGIN_DENIED:
+      case CURLE_TFTP_PERM:
+      case CURLE_TFTP_NOSUCHUSER:
+      case CURLE_SSL_ISSUER_ERROR:
+        return LIST_ERROR_AUTHENTICATION;
+
+      case CURLE_FAILED_INIT:
+      case CURLE_FTP_CANT_GET_HOST:
+      case CURLE_OUT_OF_MEMORY:
+      case CURLE_FUNCTION_NOT_FOUND:
+      case CURLE_BAD_FUNCTION_ARGUMENT:
+      case CURLE_UNKNOWN_OPTION:
+      case CURLE_SSL_ENGINE_NOTFOUND:
+      case CURLE_SSL_ENGINE_SETFAILED:
+      case CURLE_SSL_ENGINE_INITFAILED:
+      case CURLE_CONV_FAILED:
+      case CURLE_CONV_REQD:
+      case CURLE_AGAIN:
+      case CURLE_CHUNK_FAILED:
+      case CURLE_NO_CONNECTION_AVAILABLE:
+      case CURLE_OBSOLETE16:
+      case CURLE_OBSOLETE20:
+      case CURLE_OBSOLETE24:
+      case CURLE_OBSOLETE29:
+      case CURLE_OBSOLETE32:
+      case CURLE_OBSOLETE40:
+      case CURLE_OBSOLETE44:
+      case CURLE_OBSOLETE46:
+      case CURLE_OBSOLETE50:
+      case CURLE_OBSOLETE57:
+      case CURL_LAST:
+        break;
+    }
+
+    return LIST_ERROR_INTERNAL;
+}
+
 /*!
  * Perform download of given URL, write to file with given name.
  *
@@ -170,7 +283,7 @@ static enum DBusListsErrorCode do_download(const struct XferItem *const item,
         .previously_sent_tick = UINT32_MAX,
     };
 
-    char error_buffer[CURL_ERROR_SIZE] = "(details unknown)";
+    char error_buffer[CURL_ERROR_SIZE] = "[details unknown]";
 
     curl_easy_setopt(rx, CURLOPT_URL, item->url);
     curl_easy_setopt(rx, CURLOPT_FOLLOWLOCATION, 1L);
@@ -186,8 +299,7 @@ static enum DBusListsErrorCode do_download(const struct XferItem *const item,
     curl_easy_setopt(rx, CURLOPT_ERRORBUFFER, error_buffer);
 
     const CURLcode rx_result = curl_easy_perform(rx);
-    enum DBusListsErrorCode error =
-        (rx_result == CURLE_OK) ? LIST_ERROR_OK : LIST_ERROR_NET_IO;
+    enum DBusListsErrorCode error = map_curl_error_to_list_error(rx_result);
 
     curl_easy_cleanup(rx);
     rx = NULL;
@@ -206,7 +318,8 @@ static enum DBusListsErrorCode do_download(const struct XferItem *const item,
     else
     {
         if(rx_result != CURLE_ABORTED_BY_CALLBACK)
-            msg_error(0, LOG_ERR, "Failed downloading file: %s", error_buffer);
+            msg_error(0, LOG_ERR, "Failed downloading file: %s (%s)",
+                      error_buffer, curl_easy_strerror(rx_result));
         else
         {
             msg_info("Download canceled as requested");
